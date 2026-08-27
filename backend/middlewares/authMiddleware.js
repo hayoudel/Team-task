0ba0153b0f9dffe35b530757f80958fba02f0d6c
Imac de Delphine <imacdedelphine@iMac-de-Ingenieur.local>
2026-08-27 19:29:00 +0000

    ajout du authmiddleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  try {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Vous devez être connecté"
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET 
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+
+    return res.status(401).json({
+      message: "Token invalide ou expiré"
+    });
+
+  }
+};
+
--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
 import {createProjectController,getAllProjectController, getProjectByIdController,updateProjectController,deleteProjectController} from "../controllers/projetControllers.js"
-
+import { verifyToken } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/",createProjectController );
-router.get("/", getAllProjectController);
+router.get("/",verifyToken, getAllProjectController);
 router.get("/:id",getProjectByIdController);
 router.put("/:id",updateProjectController);
 router.delete("/:id",deleteProjectController);
